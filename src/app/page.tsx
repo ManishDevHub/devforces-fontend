7d791eb24375dbad82bdf324970c32f500d771fd
YourGitHubUsername <ramuk13476@gmail.com>
2026-01-30 19:02:13 +0530

    navbar component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Navbar from '@/components/navbar'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>Home page </div>
+    <div>
+
+      <Navbar></Navbar>
+      <div className='flex items-center justify-center bg-red-400'>
+        <h5 className=' '>Season 4 Contest Now Live</h5>
+        <h1>Compete. Build. Dominate</h1>
+        <p>The ultimate competitive programming platform for web <br /> Solve real-world challenges, build production-grade systems, and prove <br /> your skills against the best developers worldwide.</p>
+        <div>
+          <Button> Get Started</Button>
+          <Button> View Contests</Button>
+        </div>
+      </div>
+     
+    </div>
   )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from 'react'
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <div className=" w-full bg-background border-b ">
+        <div  className="mx-auto h-14 m-w-xl flex items-center justify-between px-5 ">
+         
+            <Link className="text-xl font-bold" href="/">DevForce</Link>
+            <div className=" hidden md:flex items-center gap-10">
+                <Link className="text-sm font-medium hover:text-primary " href="/contests"> Contest</Link>
+                <Link className="text-sm font-medium hover:text-primary " href="/problems"> Problems</Link>
+                <Link className="text-sm font-medium hover:text-primary " href="/practice"> Practice</Link>
+                <Link className="text-sm font-medium hover:text-primary " href="/leaderboard">LeaderBoard</Link>
+                 <Link className="text-sm font-medium hover:text-primary " href="/discuss">Discuss</Link>
+
+            </div>
+            <div className="flex items-center gap-2">
+                <Button className="flex items-center gap-2" variant="default" > Get Started</Button>
+                  <Button className="flex items-center gap-2 " variant="sign"> SignUp</Button>
+            </div>
+         
+        </div>
+
+    </div>
+  )
+}
